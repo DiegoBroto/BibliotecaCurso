602973dcc3e9e14cbceb837665e1609002365552
DiegoBroto <diegobroto82@gmail.com>
2023-07-07 18:07:46 -0300

    tercer commit

--- src/main/java/BibliotecaNacional/Libro.java
+++ src/main/java/BibliotecaNacional/Libro.java
@@ -1,5 +1,7 @@
 package BibliotecaNacional;
 
+import java.util.Objects;
+
 public class Libro {
 
     private final String titulo;
@@ -16,5 +18,28 @@ public class Libro {
 
     public Integer getIsbn() {
         return isbn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Libro)) return false;
+        Libro libro = (Libro) o;
+        return Objects.equals(getIsbn(), libro.getIsbn());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIsbn());
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", isbn=" + isbn +
+                ", autor='" + autor + '\'' +
+                ", categoria='" + categoria + '\'' +
+                '}';
     }
 }
